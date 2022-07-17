a5290237bec1701a4ba147a87b6d754dab8a8861
Beau McCartney <mccartney.beausl@gmail.com>
2022-07-17 17:25:04 -0600

    Add tools-y plugins

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -32,4 +32,44 @@ function config.telescope()
   require('telescope').load_extension('fzy_native')
 end
 
+function config.dap()
+  local dap = require('dap')
+
+  dap.adapters.codelldb = {
+    type = 'server',
+    port = "${port}",
+    executable = {
+      command = CODELLDB_PATH,
+      args = {"--port", "${port}"},
+    }
+  }
+  dap.configurations.c = {
+    {
+      name = "Launch file",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      stopOnEntry = true,
+    },
+  }
+  dap.configurations.cpp = dap.configurations.c
+end
+
+function config.dapui()
+  local dap, dapui = require("dap"), require("dapui")
+  dapui.setup()
+  dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+  end
+  dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+  end
+  dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+  end
+end
+
 return config
--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -15,3 +15,65 @@ plugin({
     { 'nvim-telescope/telescope-fzy-native.nvim', opt = true },
   },
 })
+
+plugin ({
+  'gigablaster/rust-tools.nvim',
+  requires = { 'nvim-lua/plenary.nvim', 'mfussenegger/nvim-dap' },
+})
+
+plugin ({
+  'mfussenegger/nvim-dap',
+  config = conf.dap,
+})
+
+plugin ({
+  'rcarriga/nvim-dap-ui',
+  requires = 'mfussenegger/nvim-dap',
+  config = conf.dapui
+})
+
+plugin ({
+  'theHamsta/nvim-dap-virtual-text',
+  requires = { 'mfussenegger/nvim-dap', 'nvim-treesitter' },
+  config = function()
+    require('nvim-dap-virtual-text').setup()
+  end
+})
+
+plugin ({
+  'szw/vim-maximizer',
+  config = function()
+    vim.g.maximizer_set_default_mapping = 0
+  end
+})
+
+plugin ({
+  'thePrimeagen/harpoon',
+  requires = 'nvim-lua/plenary.nvim',
+})
+
+plugin 'mg979/vim-visual-multi'
+
+plugin 'tpope/vim-fugitive'
+
+plugin ({ 'junegunn/gv.vim', requires = 'vim-fugitive' })
+
+plugin ({ 'tpope/vim-rhubarb', requires = 'vim-fugitive' })
+
+plugin ({ 'shumphrey/fugitive-gitlab.vim', requires = 'vim-fugitive' })
+
+plugin ({
+  'sindrets/diffview.nvim',
+  requires = 'nvim-lua/plenary.nvim',
+  config = function()
+    require('diffview').setup { use_icons = false, }
+  end,
+})
+
+plugin ({
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup()
+  end
+})
+
