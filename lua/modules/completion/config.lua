2cb5d432836c7ed1ce70193d53555578d20634ab
Beau McCartney <mccartney.beausl@gmail.com>
2022-07-17 16:42:44 -0600

    Add completion-y plugins

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -4,18 +4,99 @@
 
 local config = {}
 
--- config server in this function
-function config.nvim_lsp() end
+function config.navigator()
+  -- nvim-autopairs
+  require('nvim-autopairs').setup { disable_filetype = { "TelescopePrompt", "guihua", "guihua_rust", "clap_input" }, }
+
+  -- cmp
+  if vim.o.ft == 'clap_input' and vim.o.ft == 'guihua' and vim.o.ft == 'guihua_rust' then
+    require('cmp').setup.buffer { completion = { enable = false } }
+  end
+
+  require('navigator').setup {
+    lsp = {
+      code_lens_action = { enable = false },
+      servers = {
+        'cssmodules_ls',
+        'eslint',
+        'tailwindcss',
+      },
+      disable_lsp = { "rust_analyzer", "clangd" },
+      format_on_save = false,
+    },
+    lsp_signature_help = true,
+    signature_help_cfg = require('lsp_signature').setup {
+      always_trigger = true,
+    }
+  }
+
+  local opts = {
+    dap = {
+      adapter = require('rust-tools.dap').get_codelldb_adapter(
+        CODELLDB_PATH, LIBLLDB_PATH)
+    },
+    server = {
+      on_attach = function(client, bufnr)
+        require('navigator.lspclient.mapping').setup({ client = client, bufnr = bufnr })
+      end,
+    }
+  }
+
+  require('rust-tools').setup(opts)
+
+  require("clangd_extensions").setup {
+    server = {
+      on_attach = function(client, bufnr)
+        require('navigator.lspclient.mapping').setup({ client = client, bufnr = bufnr })
+      end,
+    }
+  }
+end
+
+function config.null_ls()
+  local nls = require('null-ls')
+  nls.setup {
+    sources = {
+      nls.builtins.code_actions.gitsigns,
+      nls.builtins.code_actions.shellcheck,
+      nls.builtins.diagnostics.eslint,
+      nls.builtins.diagnostics.fish,
+      nls.builtins.formatting.rustfmt,
+    }
+  }
+end
 
 function config.nvim_cmp()
   local cmp = require('cmp')
 
+  cmp.event:on(
+    'confirm_done',
+    require('nvim-autopairs.completion.cmp').on_confirm_done()
+  )
   cmp.setup({
     preselect = cmp.PreselectMode.Item,
     window = {
       completion = cmp.config.window.bordered(),
       documentation = cmp.config.window.bordered(),
     },
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    sources = {
+      { name = 'crates' },
+      { name = 'npm' },
+      { name = 'conventionalcommits' },
+      { name = 'latex_symbols' },
+      { name = 'fish' },
+      { name = 'nvim_lua' },
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+      { name = 'path' },
+      { name = 'buffer' }
+    }
   })
 end
 
@@ -39,6 +120,13 @@ function config.lua_snip()
   require('luasnip.loaders.from_vscode').lazy_load({
     paths = { './snippets/' },
   })
+  -- snippet navigation
+  vim.cmd([[
+    imap <silent><expr> <C-k> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<C-k>'
+    inoremap <silent> <C-j> <cmd>lua require('luasnip').jump(-1)<Cr>
+    snoremap <silent> <C-k> <cmd>lua require('luasnip').jump(1)<Cr>
+    snoremap <silent> <C-j> <cmd>lua require('luasnip').jump(-1)<Cr>
+  ]])
 end
 
 return config
--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -4,13 +4,72 @@
 
 local plugin = require('core.pack').register_plugin
 local conf = require('modules.completion.config')
+local filetypes = {
+  'html',
+  'css',
+  'javascript',
+  'java',
+  'javascriptreact',
+  'vue',
+  'typescript',
+  'typescriptreact',
+  'go',
+  'lua',
+  'cpp',
+  'c',
+  'markdown',
+  'makefile',
+  'python',
+  'bash',
+  'sh',
+  'php',
+  'yaml',
+  'json',
+  'sql',
+  'vim',
+  'sh',
+  'tex',
+  'svelte',
+  'nim'
+}
 
 plugin({
   'neovim/nvim-lspconfig',
   -- used filetype to lazyload lsp
   -- config your language filetype in here
-  ft = { 'lua', 'rust', 'c', 'cpp' },
-  config = conf.nvim_lsp,
+  ft = filetypes,
+})
+
+plugin({ 'windwp/nvim-autopairs' })
+
+plugin ({
+  'windwp/nvim-ts-autotag',
+  requires = 'nvim-treesitter',
+  config = function()
+    require('nvim-ts-autotag').setup()
+  end,
+})
+
+plugin {
+  'ray-x/navigator.lua',
+  requires = {
+    { 'ray-x/guihua.lua', run = 'cd lua/fzy && make' },
+    'neovim/nvim-lspconfig',
+    'ray-x/lsp_signature.nvim',
+    'gigablaster/rust-tools.nvim', -- TODO: go back to original when https://github.com/simrat39/rust-tools.nvim/issues/157 is fixed
+    'p00f/clangd_extensions.nvim',
+  },
+  -- TODO: set filetypes 
+  -- texlab, rust_analyzer, clangd, pyright, tailwindcss, eslint, cssmodules_ls, html, tsserver, jdtls, svelte, sqls, gopls, sumneko_lua, bashls, vimls, jsonls, cssls, yamlls, nimls, vuels, phpactor
+  -- ^^ configured lsp's for reference
+  ft = filetypes,
+  config = conf.navigator,
+  commit = '05753da8db0e7fdb979cc5fc396965b150e09d79',
+}
+
+plugin ({
+  'jose-elias-alvarez/null-ls.nvim',
+  config = conf.null_ls,
 })
 
 plugin({
@@ -18,11 +77,21 @@ plugin({
   event = 'BufReadPre',
   config = conf.nvim_cmp,
   requires = {
+    { 'hrsh7th/cmp-nvim-lua', },
+    { 'mtoohey31/cmp-fish', },
+    { 'kdheepak/cmp-latex-symbols', },
     { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-lspconfig' },
     { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
     { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
     { 'saadparwaiz1/cmp_luasnip', after = 'LuaSnip' },
+
+    { "David-Kunz/cmp-npm", requires = "nvim-lua/plenary.nvim",
+      config = function() require('cmp-npm').setup() end, },
+    { "saecki/crates.nvim",
+      requires = "nvim-lua/plenary.nvim",
+      config = function() require('crates').setup() end,
+    },
   },
 })
 
-plugin({ 'L3MON4D3/LuaSnip', event = 'InsertEnter', config = conf.lua_snip })
+plugin({ 'L3MON4D3/LuaSnip', event = 'InsertEnter', config = conf.lua_snip }) -- friendly snippets?
