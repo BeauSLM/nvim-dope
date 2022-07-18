02f4ba147247b26415309b349b9dab77fdf2a4f3
Beau McCartney <mccartney.beausl@gmail.com>
2022-07-18 12:06:50 -0600

    idk why treesitter is after telescope

--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -14,7 +14,7 @@ plugin({
   requires = {
     { 'nvim-lua/popup.nvim', opt = true },
     { 'nvim-lua/plenary.nvim', opt = true },
-    { 'nvim-telescope/telescope-fzy-native.nvim', opt = true },
+    'nvim-telescope/telescope-fzy-native.nvim',
   },
 })
 
