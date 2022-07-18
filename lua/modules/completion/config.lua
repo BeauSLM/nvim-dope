1fd517bab04ffae549c66838ffb737b16bc17e2f
Beau McCartney <mccartney.beausl@gmail.com>
2022-07-18 12:03:43 -0600

    add cmp mapping

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -63,6 +63,14 @@ function config.nvim_cmp()
         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       end,
     },
+    mapping = {
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<C-n>'] = cmp.mapping.select_next_item(),
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+    },
     sources = {
       { name = 'crates' },
       { name = 'npm' },
