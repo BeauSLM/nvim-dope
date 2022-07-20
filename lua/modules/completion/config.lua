e4ed551058a40d347fdabf9dcb235868634e8715
Beau McCartney <mccartney.beausl@gmail.com>
2022-07-20 11:33:43 -0600

    cmp non-fancy window

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -54,10 +54,6 @@ function config.nvim_cmp()
 
   cmp.setup({
     preselect = cmp.PreselectMode.Item,
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
