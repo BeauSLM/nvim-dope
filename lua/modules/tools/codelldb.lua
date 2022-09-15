00d25298861b92c5b429b61be167150803c67ab0
Beau McCartney <beau@beaumccartney.xyz>
2022-09-15 11:16:45 -0600

    new codelldb version

--- lua/modules/tools/codelldb.lua
+++ lua/modules/tools/codelldb.lua
@@ -1,5 +1,5 @@
 local codelldb = {}
-local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.7.3/'
+local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.7.4/'
 codelldb.bin_path = extension_path .. 'adapter/codelldb'
 codelldb.lib_path = extension_path .. 'lldb/lib/liblldb.so'
 
